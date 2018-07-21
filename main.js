c7c2eb8498f7c60b2b1d5b2e5b61ddc560f7fdfc
hoboaki <hoboaki@10106.net>
2018-07-22 00:19:15 +0900

    refactor: フォルダ移動。

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function createWindow () {
   //mainWindow.setMenuBarVisibility(false);
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('src/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
