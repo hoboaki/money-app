6416e7d11061e0c9fe20461a997fbc9c11dddc59
hoboaki <hoboaki@10106.net>
2018-08-05 01:40:55 +0900

    feat: コンポーネントを使用。

--- new-src/ts/index.tsx
+++ new-src/ts/index.tsx
@@ -1,9 +1,19 @@
+// インポート。
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './App';
 
+// 全 css ファイルを require して watch 対応。
+// function requireAll(r: any) {
+//   r.keys().forEach(r);
+// }
+// requireAll((require as any).context('./', true, /\.css$/));
+
+// 描画
 const container = document.getElementById('contents');
-
 ReactDom.render(
-    <p>こんにちは、世界</p>,
-    container,
+  <App/>,
+  container,
 );
+
+// EOF
