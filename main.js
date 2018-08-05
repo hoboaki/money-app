7b2bf8ae55242f6b64d37a437eeb103cc57a1b86
hoboaki <hoboaki@10106.net>
2018-08-05 15:00:10 +0900

    feat: main.js を移植。

--- main.js
+++ main.js
@@ -1,51 +1,82 @@
-const {
-    app,
-    BrowserWindow
-} = require('electron')
+/**
+ * @fileOverview
+ * エントリーポイント。
+ */
 
-// レンダープロセスとなるブラウザ・ウィンドウのオブジェクト。
-// オブジェクトが破棄されると、プロセスも終了するので、グローバルオブジェクトとする。
-let win
+// Modules to control application life and create native browser window
+const {app, globalShortcut, BrowserWindow} = require('electron')
 
-function createWindow() {
-    // ブラウザウィンドウの作成
-    win = new BrowserWindow({
-        width: 800,
-        height: 600
-    })
-    // index.html をロードする
-    win.loadFile('public/index.html')
-    // 起動オプションに、 "--debug"があれば開発者ツールを起動する
-    if (process.argv.find((arg) => arg === '--debug')) {
-        win.webContents.openDevTools()
-    }
-    // ブラウザウィンドウを閉じたときのイベントハンドラ
-    win.on('closed', () => {
-        // 閉じたウィンドウオブジェクトにはアクセスできない
-        win = null
-    })
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1000, 
+    height: 600, 
+    titleBarStyle: 'hidden'
+  })
+  //mainWindow.setMenuBarVisibility(false);
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('public/index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+
+  mainWindow.on('focus', function () {
+    mainWindow.webContents.send('app-message', 'focus');
+  })
+  mainWindow.on('blur', function () {
+    mainWindow.webContents.send('app-message', 'blur');
+  })
 }
 
-// このメソッドは、Electronが初期化を終了し、
-// ブラウザウィンドウを作成する準備ができたら呼び出される。
-// 一部のAPIは、このイベントが発生した後にのみ使用できる。
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// 全てのウィンドウオブジェクトが閉じたときのイベントハンドラ
-app.on('window-all-closed', () => {
-    // macOSでは、アプリケーションとそのメニューバーがCmd + Qで
-    // 明示的に終了するまでアクティブになるのが一般的なため、
-    // メインプロセスは終了させない
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-});
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
-app.on('activate', () => {
-    // MacOSでは、ドックアイコンがクリックされ、
-    // 他のウィンドウが開いていないときに、アプリケーションでウィンドウを
-    // 再作成するのが一般的です。
-    if (win === null) {
-        createWindow()
-    }
-});
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// For Debug
+if (process.env.NODE_ENV==='development') {
+  app.on('browser-window-focus', (event, focusedWindow) => {
+    globalShortcut.register(
+　　　　　　　　　　　　process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+　　　　　　　　　　　　() => focusedWindow.webContents.toggleDevTools()
+　　　　　　　　)
+  })
+  app.on('browser-window-blur', (event, focusedWindow) => {
+    globalShortcut.unregister(
+　　　　　　　　　　　　process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I'
+　　　　　　　　)
+  })
+}
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
