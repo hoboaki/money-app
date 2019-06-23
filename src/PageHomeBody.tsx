3d53573f625ee5f12fd962b410b809634ca29f4a
hoboaki <hoboaki@10106.net>
2019-06-24 00:16:25 +0900

    feat: 概要表を追加。

--- src/PageHomeBody.tsx
+++ src/PageHomeBody.tsx
@@ -8,8 +8,61 @@ class PageHomeBody extends React.Component<any, any> {
     const rootClass = ClassNames(
       Style.Root,
     );
+    const abstractClass = ClassNames(
+      Style.Abstract,
+    );
+    const abstractSpacerClass = ClassNames(
+      Style.AbstractSpacer,
+    );
+    const abstractTableClass = ClassNames(
+      Style.AbstractTable,
+    );
+    const abstractTableHeaderLabelClass = ClassNames(
+      Style.AbstractTableHeader,
+      Style.AbstractTableHeaderLabel,
+    );
+    const abstractTableHeaderSpacerClass = ClassNames(
+      Style.AbstractTableHeader,
+      Style.AbstractTableHeaderSpacer,
+    );
+    const abstractTableDataValueClass = ClassNames(
+      Style.AbstractTableData,
+      Style.AbstractTableDataValue,
+    );
+    const abstractTableDataSignClass = ClassNames(
+      Style.AbstractTableData,
+      Style.AbstractTableDataSign,
+    );
     return (
-      <div className={rootClass}></div>
+      <div className={rootClass}>
+        <div className={abstractClass}>
+          <table className={abstractTableClass}>
+            <tr>
+              <th className={abstractTableHeaderLabelClass}>当月収入</th>
+              <th className={abstractTableHeaderSpacerClass}></th>
+              <th className={abstractTableHeaderLabelClass}>当月支出</th>
+              <th className={abstractTableHeaderSpacerClass}></th>
+              <th className={abstractTableHeaderLabelClass}>当月収支</th>
+            </tr>
+            <tr>
+              <td className={abstractTableDataValueClass}>¥999,999,999</td>
+              <td className={abstractTableDataSignClass}>-</td>
+              <td className={abstractTableDataValueClass}>¥999,999,999</td>
+              <td className={abstractTableDataSignClass}>=</td>
+              <td className={abstractTableDataValueClass}>¥999,999,999</td>
+            </tr>
+          </table>
+          <div className={abstractSpacerClass}/>
+          <table className={abstractTableClass}>
+            <tr>
+              <th className={abstractTableHeaderLabelClass}>残高</th>
+            </tr>
+            <tr>
+              <td className={abstractTableDataValueClass}>¥999,999,999</td>
+            </tr>
+          </table>
+        </div>
+      </div>
     );
   }
 
