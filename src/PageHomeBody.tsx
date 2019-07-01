6509d5f604da546904b100cf1d9988acd5b5d9b2
hoboaki <hoboaki@10106.net>
2019-07-01 23:25:52 +0900

    feat: モーダルダイアログの動作デモ。

--- src/PageHomeBody.tsx
+++ src/PageHomeBody.tsx
@@ -14,6 +14,29 @@ class PageHomeBody extends React.Component<any, any> {
       <div className={rootClass}>
         <PageHomeCalendar />
         <PageHomeBalance />
+        <button className="btn btn-primary" data-toggle="modal" data-target="#basicExampleModal">
+          Launch demo modal
+        </button>
+        <div className="modal fade" id="basicExampleModal" tabIndex={-1}
+          role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                ...
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
