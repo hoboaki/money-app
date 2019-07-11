8a5cd3c8376d1a568558e86d6988fba8b8880941
hoboaki <hoboaki@10106.net>
2019-07-12 00:46:30 +0900

    Revert "feat: context-menu 実装中。まだ動かない。"
    This reverts commit 2bcc5b0e1c7c58d7797d246daeda4b84d0b74c8c.

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import MainWindow from './MainWindow';
 
 // node_modules の css ロード
 import 'flatpickr/dist/themes/dark.css';
-import 'jquery-contextmenu/dist/jquery.contextMenu.min.css';
 
 // 全 css ファイルを require して watch 対応。
 function requireAll(r: any) {
