b7b1461d924faf7896f82b5a123821f419309273
hoboaki <hoboaki@10106.net>
2019-07-15 16:00:06 +0900

    refactor: 移植したファイルを削除。

--- src/Model/Doc/AccountGroup.ts
+++
@@ -1,8 +0,0 @@
-/// 口座グループ。
-enum AccountGroup {
-  Invalid = 0,
-  Assets = 1, // 資産。
-  Liabilities = 2, // 負債。
-}
-
-export default AccountGroup;
--- src/Model/Doc/CategoryKind.ts
+++
@@ -1,8 +0,0 @@
-/// カテゴリの種類。
-enum CategoryKind {
-  Invalid = 0, // 無効値。
-  Income = 1, // 入金。
-  Outgo = 2, // 出金。
-}
-
-export default CategoryKind;
