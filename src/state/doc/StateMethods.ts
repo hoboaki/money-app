03a6075b7893eb0b12cfc71f0417babf0c3b84ec
hoboaki <hoboaki@10106.net>
2019-07-15 16:21:11 +0900

    feat: StateMethods で未実装の処理を追加していく。

--- src/state/doc/StateMethods.ts
+++ src/state/doc/StateMethods.ts
@@ -1,7 +1,9 @@
 import Clone from 'clone';
 import DataAccount from '../../data-model/doc/Account';
 import DataCategory from '../../data-model/doc/Category';
+import DataRecordIncome from '../../data-model/doc/RecordIncome';
 import DataRecordOutgo from '../../data-model/doc/RecordOutgo';
+import DataRecordTransfer from '../../data-model/doc/RecordTransfer';
 import DataRoot from '../../data-model/doc/Root';
 import YearMonthDayDate from '../../util/YearMonthDayDate';
 import * as States from './States';
@@ -88,6 +90,41 @@ export const toData = (state: States.IState) => {
     data.initialAmount = src.initialAmount;
   }
 
+  // 入金
+  {
+    // カテゴリ
+    for (const key in state.income.categories) {
+      if (!state.income.categories.hasOwnProperty(key)) {
+        continue;
+      }
+      const src = state.income.categories[key];
+      const data = new DataCategory();
+      data.id = src.id;
+      data.name = src.name;
+      if (src.parent != null) {
+        data.parent = src.parent;
+      }
+      result.income.categories.push(data);
+    }
+
+    // レコード
+    for (const key in state.income.records) {
+      if (!state.income.records.hasOwnProperty(key)) {
+        continue;
+      }
+      const src = state.income.records[key];
+      const data = new DataRecordIncome();
+      data.createDate = src.createDate.toISOString();
+      data.updateDate = src.updateDate.toISOString();
+      data.date = src.date.toText();
+      data.memo = src.memo;
+      data.amount = src.amount;
+      data.category = src.category;
+      data.account = src.account;
+      result.income.records.push(data);
+    }
+  }
+
   // 出金
   {
     // カテゴリ
@@ -123,6 +160,26 @@ export const toData = (state: States.IState) => {
     }
   }
 
+  // 送金
+  {
+    // レコード
+    for (const key in state.transfer.records) {
+      if (!state.transfer.records.hasOwnProperty(key)) {
+        continue;
+      }
+      const src = state.transfer.records[key];
+      const data = new DataRecordTransfer();
+      data.createDate = src.createDate.toISOString();
+      data.updateDate = src.updateDate.toISOString();
+      data.date = src.date.toText();
+      data.memo = src.memo;
+      data.amount = src.amount;
+      data.accountFrom = src.accountFrom;
+      data.accountTo = src.accountTo;
+      result.transfer.records.push(data);
+    }
+  }
+
   // 結果を返す
   return result;
 };
@@ -151,6 +208,71 @@ export const accountAdd = (
   state.accounts[obj.id] = obj;
   return obj.id;
 };
+
+
+/// 入金カテゴリ追加。
+/// @return {number} 追加したカテゴリの CategoryId。
+export const incomeCategoryAdd = (
+  state: States.IState,
+  name: string,
+  parentId: number | null,
+  ) => {
+  // オブジェクト作成
+  const obj = {
+    id: 0,
+    name,
+    parent: parentId,
+    childs: [],
+  };
+  const parent = parentId != null ? state.income.categories[parentId] : null;
+
+  // 追加
+  obj.id = state.nextId.income.category;
+  state.nextId.income.category++;
+  state.income.categories[obj.id] = obj;
+  if (parentId != null) {
+    if (parent != null) {
+      parent.childs.push(obj.id);
+    } else {
+      throw new Error(`Category parent (id: ${parentId}) is not exist.`);
+    }
+  }
+  return obj.id;
+};
+
+/**
+ * 入金レコードの追加。
+ * @param amount 金額。(入金がプラス・出金がマイナス)
+ */
+export const incomeRecordAdd = (
+  state: States.IState,
+  createDate: Date,
+  updateDate: Date,
+  date: YearMonthDayDate,
+  memo: string,
+  accountId: number,
+  categoryId: number,
+  amount: number,
+  ) => {
+  // オブジェクト作成
+  const obj = {
+    id: 0,
+    createDate,
+    updateDate,
+    date,
+    memo,
+    account: accountId,
+    category: categoryId,
+    amount,
+  };
+
+  // 追加
+  obj.id = state.nextId.income.record;
+  state.nextId.income.record++;
+  state.income.records[obj.id] = obj;
+  return obj.id;
+};
+
 
 /// 出金カテゴリ追加。
 /// @return {number} 追加したカテゴリの CategoryId。
@@ -196,8 +318,6 @@ export const outgoRecordAdd = (
   categoryId: number,
   amount: number,
   ) => {
-  global.console.log('Called outgoRecordAdd');
-
   // オブジェクト作成
   const obj = {
     id: 0,
@@ -214,5 +334,38 @@ export const outgoRecordAdd = (
   obj.id = state.nextId.outgo.record;
   state.nextId.outgo.record++;
   state.outgo.records[obj.id] = obj;
+  return obj.id;
+};
+
+/**
+ * 送金レコードの追加。
+ * @param amount 金額。送金元口座からは減算され送金先口座に加算される。
+ */
+export const transferRecordAdd = (
+  state: States.IState,
+  createDate: Date,
+  updateDate: Date,
+  date: YearMonthDayDate,
+  memo: string,
+  accountFromId: number,
+  accountToId: number,
+  amount: number,
+  ) => {
+  // オブジェクト作成
+  const obj = {
+    id: 0,
+    createDate,
+    updateDate,
+    date,
+    memo,
+    accountFrom: accountFromId,
+    accountTo: accountToId,
+    amount,
+  };
+
+  // 追加
+  obj.id = state.nextId.transfer.record;
+  state.nextId.transfer.record++;
+  state.transfer.records[obj.id] = obj;
   return obj.id;
 };
