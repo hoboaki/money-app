48606d442998b647c49b78f78638bd4884482466
hoboaki <hoboaki@10106.net>
2019-07-28 00:43:31 +0900

    feat: YearMonthDayDate の interface 化。

---
+++ src/util/IYearMonthDayDate.ts
@@ -0,0 +1,7 @@
+interface IYearMonthDayDate {
+  year: number; // 2000年の場合 2000
+  month: number; // 1月の場合 1
+  day: number; // 1日の場合 1
+}
+
+export default IYearMonthDayDate;
---
+++ src/util/IYearMonthDayDateUtils.ts
@@ -0,0 +1,70 @@
+import IYearMonthDayDate from './IYearMonthDayDate';
+
+/** 今日で作成。 */
+export const createToday = (): IYearMonthDayDate => {
+  return fromDate(new Date());
+};
+
+/** Date オブジェクトから作成。 */
+export const fromDate = (nativeDate: Date): IYearMonthDayDate => {
+  return {
+    year: nativeDate.getFullYear(),
+    month: nativeDate.getMonth(),
+    day: nativeDate.getDate(),
+  };
+};
+
+/** YYYY-MM-DD 形式のテキストから作成。 */
+export const fromText = (text: string): IYearMonthDayDate => {
+  return fromDate(new Date(text));
+};
+
+/** Date に変換。（時刻は0時0分0秒） */
+export const toNativeDate = (date: IYearMonthDayDate): Date => {
+  return new Date(date.year, date.month - 1, date.day, 0, 0, 0, 0);
+};
+
+/** 前月の初日。 */
+export const prevMonth = (date: IYearMonthDayDate): IYearMonthDayDate => {
+  return fromDate(new Date(date.year, date.month - 2, 1));
+};
+
+/** 翌月の初日。 */
+export const nextMonth = (date: IYearMonthDayDate): IYearMonthDayDate => {
+  return fromDate(new Date(date.year, date.month, 1));
+};
+
+/** 前日。 */
+export const prevDay = (date: IYearMonthDayDate): IYearMonthDayDate => {
+  return fromDate(new Date(date.year, date.month - 1, date.day - 1));
+};
+
+/** 翌日。 */
+export const nextDay = (date: IYearMonthDayDate): IYearMonthDayDate => {
+  return fromDate(new Date(date.year, date.month - 1, date.day + 1));
+};
+
+/** 今指している月の初日。 */
+export const firstDayOfMonth = (date: IYearMonthDayDate): IYearMonthDayDate => {
+  return fromDate(new Date(date.year, date.month - 1, 1));
+};
+
+/** 今指している月の末日。 */
+export const lastDayOfMonth = (date: IYearMonthDayDate): IYearMonthDayDate => {
+  return fromDate(new Date(date.year, date.month, 0));
+};
+
+/** yyyy-mm-dd 形式に変換。 */
+export const toText = (date: IYearMonthDayDate): string => {
+  const toDoubleDigits = (num: number) => {
+    let text = String(num);
+    if (text.length === 1) {
+      text = '0' + text;
+    }
+    return text;
+  };
+  const year = date.year;
+  const month = toDoubleDigits(date.month);
+  const day = toDoubleDigits(date.day);
+  return `${year}-${month}-${day}`;
+};
