c5bfcf424b1ca7abfba2dee1d46bdb91f50ce72a
hoboaki <hoboaki@10106.net>
2019-08-02 01:08:55 +0900

    feat: mdb-modal の @types を追加。

---
+++ src/@types/mdb/modal/index.ts
@@ -0,0 +1,10 @@
+/// <reference types="jquery" />
+
+// interface JQueryStatic {
+//   contextMenu(options?: JQueryContextMenuOptions): JQuery;
+//   contextMenu(type: string, selector?: any): JQuery;
+// }
+
+interface JQuery {
+  modal(options?: any): JQuery;
+}
