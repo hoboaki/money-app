d1cb379da7b863f124d73d063ccc65f6610822a5
hoboaki <hoboaki@10106.net>
2020-01-11 00:43:45 +0900

    feat: スプリッター実装。

--- src/view/page/sheet/Body.tsx
+++ src/view/page/sheet/Body.tsx
@@ -1,6 +1,7 @@
 import ClassNames from 'classnames';
 import * as React from 'react';
 import * as ReactRedux from 'react-redux';
+import Split from 'split.js';
 
 import * as DocStateMethods from 'src/state/doc/StateMethods';
 import * as DocStates from 'src/state/doc/States';
@@ -15,9 +16,25 @@ interface IProps {
   page: UiStates.IPageSheet;
 }
 
+const idPageSheetBodyTop = 'pageSheetBodyTop';
+const idPageSheetBodyBottom = 'pageSheetBodyBottom';
+
 class Body extends React.Component<IProps, any> {
   public constructor(props: IProps) {
     super(props);
+  }
+
+  public componentDidMount() {
+    // スピリッター登録
+    Split(
+      [`#${idPageSheetBodyTop}`, `#${idPageSheetBodyBottom}`],
+      {
+        sizes: [25, 75],
+        gutterSize: 12,
+        cursor: 'row-resize',
+        direction: 'vertical',
+      },
+    );
   }
 
   public render() {
@@ -323,7 +340,7 @@ class Body extends React.Component<IProps, any> {
 
     return (
       <div className={rootClass}>
-        <div id="pageSheetBodyTop" className={Styles.BodyTop}>
+        <div id={idPageSheetBodyTop} className={Styles.BodyTop}>
           <table className={Styles.Table}>
             <tbody>
               <tr>
@@ -347,7 +364,7 @@ class Body extends React.Component<IProps, any> {
             </table>
           </section>
         </div>
-        <div id="pageSheetBodyBottom" className={Styles.BodyBotton}>
+        <div id={idPageSheetBodyBottom} className={Styles.BodyBottom}>
           <table className={headTableRecordClass}>
             <tbody>
               <tr>
