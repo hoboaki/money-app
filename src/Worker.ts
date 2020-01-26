b625055a15d98c431c54ae65669009fcc60e2a20
hoboaki <hoboaki@10106.net>
2020-01-26 22:25:15 +0900

    fix: lint 対応。

--- src/Worker.ts
+++ src/Worker.ts
@@ -6,5 +6,6 @@ ctx.postMessage({ foo: 'foo' });
 
 // Respond to message from parent thread
 ctx.addEventListener('message', (event) => {
+  // tslint:disable-next-line:no-console
   console.log(event);
 });
