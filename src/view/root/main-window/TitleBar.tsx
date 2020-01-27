ae444aa1a3249cd96d8caa25e0eef971ee7488e9
hoboaki <hoboaki@10106.net>
2020-01-28 00:35:26 +0900

    feat: タイトルバーにファイル名を表示。

--- src/view/root/main-window/TitleBar.tsx
+++ src/view/root/main-window/TitleBar.tsx
@@ -1,14 +1,21 @@
 import ClassNames from 'classnames';
 import * as React from 'react';
+import * as ReactRedux from 'react-redux';
 
+import IStoreState from 'src/state/IStoreState';
+import * as UiStates from 'src/state/ui/States';
 import * as Styles from './TitleBar.css';
 
 interface IProps {
   isActive: boolean;
 }
 
-class TitleBar extends React.Component<IProps, any> {
-  public constructor(props: IProps) {
+interface ILocalProps extends IProps {
+  ui: UiStates.IState;
+}
+
+class TitleBar extends React.Component<ILocalProps, any> {
+  public constructor(props: ILocalProps) {
     super(props);
   }
 
@@ -19,13 +26,23 @@ class TitleBar extends React.Component<IProps, any> {
       {[Styles.BgActive]: this.props.isActive},
     );
     const titleClass = ClassNames(Styles.Base, Styles.Title);
+    const titleText = this.props.ui.document.filePath.length === 0 ?
+      'スタートページ' : this.props.ui.document.filePath.slice(this.props.ui.document.filePath.lastIndexOf('/') + 1);
     return (
       <div id="titleBar" className={rootClass}>
-        <div className={titleClass}><div>{'\uD83D\uDC27'} ファイル名 - MoneyApp</div></div>
+        <div className={titleClass}><div>{`\uD83D\uDC27 ${titleText}`}</div></div>
       </div>
     );
   }
-
 }
 
-export default TitleBar;
+const mapStateToProps = (state: IStoreState, props: IProps) => {
+  const result: ILocalProps = Object.assign(
+    props,
+    {
+      ui: state.ui,
+    },
+  );
+  return result;
+};
+export default ReactRedux.connect(mapStateToProps)(TitleBar);
