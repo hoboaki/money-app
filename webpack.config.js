de115ee7d910ac49bfebf3d22e5a6017a354de65
hoboaki <hoboaki@10106.net>
2020-02-02 00:03:19 +0900

    feat: prod モードでビルド設定変更。

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const hardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
-module.exports = {
+module.exports = env => {
+  return {
     // node.js で動作することを指定する
     target: 'electron-renderer',
     // 起点となるファイル
@@ -10,9 +11,9 @@ module.exports = {
     cache: true,
     // development は、 source map file を作成、再ビルド時間の短縮などの設定となる
     // production は、コードの圧縮やモジュールの最適化が行われる設定となる
-    mode: 'development', // "production" | "development" | "none"
+    mode: env.prod ? 'production' : 'development',
     // ソースマップのタイプ
-    devtool: 'source-map',
+    devtool: env.prod ?  '' : 'source-map',
     // 出力先設定 __dirname は node でのカレントディレクトリのパスが格納される変数
     output: {
         path: path.join(__dirname, 'public'),
@@ -73,4 +74,5 @@ module.exports = {
     plugins: [
       new hardSourceWebpackPlugin()
     ]
+  }
 };
