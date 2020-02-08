9096a15dc0970198dec34df461e20daf9ad1dd02
hoboaki <hoboaki@10106.net>
2020-02-09 00:10:03 +0900

    feat: 設定ファイルの format。

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,31 +5,31 @@ const ERROR = 2;
 
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint"
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
   ],
-  plugins: [
-    "@typescript-eslint"
-  ],
+  plugins: ['@typescript-eslint'],
   env: { node: true, es6: true },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: "module",
-    project: "./tsconfig.json"
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
   settings: {
     react: {
-      "pragma": "React",
-      "version": "detect",
+      pragma: 'React',
+      version: 'detect',
     },
   },
   rules: {
-    quotes: [ERROR, "single"],
-    "@typescript-eslint/explicit-function-return-type": [OFF],
-    "@typescript-eslint/interface-name-prefix": [OFF],
-  }
+    'no-constant-condition': [OFF],
+    quotes: [ERROR, 'single'],
+    '@typescript-eslint/explicit-function-return-type': [OFF],
+    '@typescript-eslint/interface-name-prefix': [OFF],
+    '@typescript-eslint/no-use-before-define': [OFF],
+  },
 };
