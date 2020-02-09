33562e0c280592482b300d710171a20ebe7ce82f
hoboaki <hoboaki@10106.net>
2020-02-09 23:41:34 +0900

    feat: インポートページを作成。（設定ページからコピー）

---
+++ src/view/page/import/Main.tsx
@@ -0,0 +1,40 @@
+import ClassNames from 'classnames';
+import * as React from 'react';
+
+import * as PageStyles from '../Page.css';
+import * as Styles from './Main.css';
+import SettingBtn from './SettingBtn';
+
+class Main extends React.Component {
+  public static PageId = 'Import';
+
+  public render() {
+    const rootClass = ClassNames(PageStyles.Base, Styles.Root);
+
+    const btnInfos = [];
+    btnInfos.push({ settingId: 'Account', title: '口座管理（準備中）', iconName: 'payment' });
+    btnInfos.push({ settingId: 'Category', title: 'カテゴリ管理（準備中）', iconName: 'class' });
+
+    const btns: JSX.Element[] = [];
+    btnInfos.forEach((btnInfo) => {
+      btns.push(
+        <SettingBtn
+          key={btnInfo.settingId}
+          onClicked={() => {
+            this.onClicked(btnInfo.settingId);
+          }}
+          title={btnInfo.title}
+          iconName={btnInfo.iconName}
+        />,
+      );
+    });
+
+    return <div className={rootClass}>{btns}</div>;
+  }
+
+  private onClicked(settingId: string) {
+    // ...
+  }
+}
+
+export default Main;
---
+++ src/view/page/import/SettingBtn.tsx
@@ -0,0 +1,30 @@
+import ClassNames from 'classnames';
+import * as React from 'react';
+
+import * as BasicStyles from '../../Basic.css';
+import * as Styles from './SettingBtn.css';
+
+interface IProps {
+  onClicked: () => void;
+  title: string;
+  iconName: string;
+}
+
+class SideBarBtn extends React.Component<IProps> {
+  public constructor(props: IProps) {
+    super(props);
+  }
+
+  public render() {
+    const rootClass = ClassNames(Styles.Root, BasicStyles.StdBtnPrimary);
+    const iconClass = ClassNames('material-icons', 'md-36', 'md-dark');
+    return (
+      <button className={rootClass} onClick={this.props.onClicked} disabled={true} title={this.props.title}>
+        <i className={iconClass}>{this.props.iconName}</i>
+        <span>{this.props.title}</span>
+      </button>
+    );
+  }
+}
+
+export default SideBarBtn;
