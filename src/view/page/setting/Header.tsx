fcd9507c5ab97af460211d9e778978a1178a971b
hoboaki <hoboaki@10106.net>
2020-02-28 23:11:43 +0900

    feat: ヘッダから戻るボタンを削除。

--- src/view/page/setting/Header.tsx
+++ src/view/page/setting/Header.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import * as ReactRedux from 'react-redux';
 import IStoreState from 'src/state/IStoreState';
 import * as UiStates from 'src/state/ui/States';
-import * as BasicStyles from 'src/view/Basic.css';
 import MaterialIcon from 'src/view/widget/material-icon';
 
 import * as Styles from './Header.css';
@@ -21,26 +20,13 @@ class Header extends React.Component<IProps> {
 
   public render() {
     const rootClass = ClassNames(Styles.Root);
-    const backBtnClass = ClassNames(BasicStyles.StdBtnPrimary, Styles.Btn, Styles.BackBtn);
 
     return (
       <div className={rootClass}>
-        <div className={Styles.SideSpace}>
-          <button className={backBtnClass} onClick={(e) => this.onMovePrevBtnPushed(e)}>
-            <MaterialIcon name="chevron_left" classNames={[]} darkMode={true} />
-          </button>
-        </div>
-        <div className={Styles.CenterSpace}>
-          <MaterialIcon name={this.props.iconName} classNames={[]} darkMode={false} />
-          <span>{this.props.title}</span>
-        </div>
-        <div className={Styles.SideSpace}></div>
+        <MaterialIcon name={this.props.iconName} classNames={[]} darkMode={false} />
+        <span>{this.props.title}</span>
       </div>
     );
-  }
-
-  private onMovePrevBtnPushed(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
-    e.stopPropagation();
   }
 }
 
