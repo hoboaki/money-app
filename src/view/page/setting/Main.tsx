e73336c8579a62a2efa26a1d6cc721a1ec451298
hoboaki <hoboaki@10106.net>
2020-02-28 23:13:44 +0900

    feat: 設定ページの不要なコードを削除。

--- src/view/page/setting/Main.tsx
+++ src/view/page/setting/Main.tsx
@@ -1,15 +1,9 @@
-import ClassNames from 'classnames';
 import * as React from 'react';
 import * as ReactRedux from 'react-redux';
 import IStoreState from 'src/state/IStoreState';
-import Store from 'src/state/Store';
-import * as UiActions from 'src/state/ui/Actions';
 import * as UiStates from 'src/state/ui/States';
-import * as PageStyles from 'src/view/page/Page.css';
 
 import Account from './Account';
-import * as Styles from './Main.css';
-import SettingBtn from './SettingBtn';
 
 interface IProps {
   states: UiStates.IPageSetting;
@@ -29,46 +23,8 @@ class Main extends React.Component<IProps> {
       case Main.SubPageIdAccount:
         return <Account />;
       default:
-        return this.renderTopPage();
+        return <div />;
     }
-  }
-
-  private renderTopPage() {
-    const rootClass = ClassNames(PageStyles.Base, Styles.Root);
-
-    const btnInfos = [];
-    btnInfos.push({ subPageId: Main.SubPageIdAccount, title: '口座管理', iconName: 'payment', disabled: false });
-    btnInfos.push({
-      subPageId: Main.SubPageIdCategory,
-      title: 'カテゴリ管理（準備中）',
-      iconName: 'class',
-      disabled: true,
-    });
-
-    const btns: JSX.Element[] = [];
-    btnInfos.forEach((btnInfo) => {
-      btns.push(
-        <SettingBtn
-          key={btnInfo.subPageId}
-          onClicked={() => {
-            this.onClicked(btnInfo.subPageId);
-          }}
-          title={btnInfo.title}
-          iconName={btnInfo.iconName}
-          disabled={btnInfo.disabled}
-        />,
-      );
-    });
-
-    return (
-      <div className={rootClass}>
-        <div className={Styles.GridHolder}>{btns}</div>
-      </div>
-    );
-  }
-
-  private onClicked(subPageId: string) {
-    Store.dispatch(UiActions.settingUpdateSubPage(subPageId));
   }
 }
 
